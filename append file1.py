b0165f3becc2f25fff09210ba4000fa00e597e54
kartik salunkhe <kartiksalunkhe2801@gmail.com>
2026-08-08 22:00:01 +0530

    Add files via upload

---
+++ append file1.py
@@ -0,0 +1,5 @@
+st="append this line add in last"
+
+f=open("file2.txt","a")
+f.write(st)
+f.close()
---
+++ avg using function.py
@@ -0,0 +1,15 @@
+def avg():
+    a=int(input("enter a number:"))
+    b=int(input("enter a number:"))
+    c=int(input("enter a number:"))
+
+    average=(a+b+c)/3
+
+    print(average)
+
+
+
+avg()
+avg()
+avg()
+avg()
