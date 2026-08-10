b5a12066ec0253b7582deaff1021d772e170035a
kartik salunkhe <kartiksalunkhe2801@gmail.com>
2026-08-10 21:01:20 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,13 @@
+class employee:
+    language="python"
+    salary=12000000
+
+
+pavan = employee()
+pavan.name="pavan"
+print(pavan.language,pavan.salary,pavan.name)
+
+kartik = employee()
+kartik.name="kartik"
+
+print(kartik.name,kartik.language,kartik.salary)
